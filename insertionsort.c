2869da492bddf5681fd91a38f6562c1870b5f13e
Arpan Das <arpan.saraswatya@gmail.com>
2026-09-19 19:12:41 +0530

    Add files via upload

---
+++ insertionsort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void insertionSort(int array[],int size){
+    for(int step=1;step<size;step++){
+        int key = array[step];
+        int j=step-1;
+        while(j>=0 && key<array[j]){
+            array[j+1]=array[j];
+            --j;
+        }
+        array[j+1]=key;
+    }
+}
+int main()
+{
+    int data[] = {6, 4, 7, 8, 1, 9, 5};
+    int size = sizeof(data) / sizeof(data[0]);
+    int i;
+
+    insertionSort(data, size);
+
+    printf("Sorted array:\n");
+
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ", data[i]);
+    }
+
+    return 0;
+}
